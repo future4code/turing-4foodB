93c98b241f858b48ec2738bafc513c552ef1a454
Diego Molinari <63561210+diegotjmolinari@users.noreply.github.com>
2020-08-12 14:00:33 -0300

    diego-duvidas

--- src/components/4. Tela Cadastro Endereço/TelaCadastroEnd.js
+++ src/components/4. Tela Cadastro Endereço/TelaCadastroEnd.js
@@ -1,144 +1,132 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import './styles.js';
 import logoinvert from '../../assets/imagens/logoinvert.png';
-import {LogoContainer, Container, FormContainerCadastroEndereco, TituloCadastroEnd, InputsCadastroEndereco, BotaoCadastroEndereco, LinkCadastro,
+import {
+  LogoContainer,
+  FormContainerCadastroEndereco,
+  TituloCadastroEnd,
+  InputsCadastroEndereco,
+  BotaoCadastroEndereco,
+  LinkCadastro,
+} from './styles';
 import AppHeader from '../AppHeader';
 
+//quando actualizar cada uno de esos campos tambem quero atulizar meu form
 
-   //quando actualizar cada uno de esos campos tambem quero atulizar meu form
+const TelaCadastroEnd = () => {
+  const history = useHistory();
+  const irParaFeed = () => {
+    history.push('/feed');
+  };
 
-      
+  const baseUrl =
+    'https://us-central1-missao-newton.cloudfunctions.net/fourFoodB';
 
-  
-        
+  const [form, setForm] = useState({
+    rua: '',
+    numero: 0,
+    nomebloco: '',
+    bairro: '',
+    cidade: '',
+    estado: '',
+  });
 
+  const onChangeInput = (event) => {
+    const newValue = event.target.value;
+    const fielName = event.target.name;
 
-    const TelaCadastroEnd = () => {
+    setForm({ ...form, [fielName]: newValue });
+  };
 
-      const history = useHistory();
-      const irParaFeed = () => {
-             history.push("/feed")
-      }
+  console.log(form);
 
+  return (
+    <FormContainerCadastroEndereco>
+      <AppHeader />
+      <LogoContainer src={logoinvert} alt="logotipo ifuture" />
 
-      const baseUrl = "https://us-central1-missao-newton.cloudfunctions.net/fourFoodB"
+      <TituloCadastroEnd>Meu endereço</TituloCadastroEnd>
 
-     
-
-    const [form, setForm] = useState({
-      rua: '',
-      numero:0,
-      nomebloco:'',
-      bairro:'',
-      cidade:'',
-      estado:''
-    })
-
-
-     const onChangeInput = (event) => {
-       const newValue = event.target.value
-       const fielName = event.target.name
-
-       setForm({...form, [fielName]: newValue})
-     }
-
-     
-
-     
-
-  
-  
-  
-
-  console.log(form)
-
-
-    return (
-        <FormContainerCadastroEndereco>
-        <AppHeader />
-        <LogoContainer
-          src={logoinvert}
-          alt="logotipo ifuture"
-        />
-
-        <TituloCadastroEnd>Meu endereço</TituloCadastroEnd>
-
-        <InputsCadastroEndereco
-          required
-          id="outlined-required"
-          label="Logradouro"
-          variant="outlined"
-          value={form['rua']}
-          onChange={onChangeInput}
-          placeholder="Rua / Av."
-          name={'rua'}
-        />
-          
-        <InputsCadastroEndereco
-          className="style-input"
-          required
-          id="outlined-required"
-          label="Numero"
-          type="number"
-          variant="outlined"
-          value={form['numero']}
-          onChange={onChangeInput}
-          placeholder="Numero"
-          name={'numero'}
-        />
-      
       <InputsCadastroEndereco
-          required
-          id="outlined-required"
-          label="Complemento"
-          variant="outlined"
-          value={form['nomebloco']}
-          onChange={onChangeInput}
-          placeholder="Apto. / Bloco"
-          name={'nomebloco'}
-        />
-         
-         <InputsCadastroEndereco
-          required
-          id="outlined-required"
-          label="Bairro"
-          variant="outlined"
-          value={form['bairro']}
-          onChange={onChangeInput}
-          placeholder="Bairro"
-          name={'bairro'}
-        />
+        required
+        id="outlined-required"
+        label="Logradouro"
+        variant="outlined"
+        value={form['rua']}
+        onChange={onChangeInput}
+        placeholder="Rua / Av."
+        name={'rua'}
+      />
 
-         <InputsCadastroEndereco
-          required
-          id="outlined-required"
-          label="Cidade"
-          variant="outlined"
-          value={form['cidade']}
-          onChange={onChangeInput}
-          placeholder="Cidade"
-          name={'cidade'}
-        />
-               
-         <InputsCadastroEndereco
-          required
-          id="outlined-required"
-          label="Estado"
-          variant="outlined"
-          value={form['estado']}
-          onChange={onChangeInput}
-          placeholder="Estado"
-          name={'estado'}
-        />
-            
-        <BotaoCadastroEndereco variant="contained" color="primary" onClick={irParaFeed}>Salvar</BotaoCadastroEndereco>
-        <LinkCadastro to="/cadastro">
-        </LinkCadastro>
+      <InputsCadastroEndereco
+        className="style-input"
+        required
+        id="outlined-required"
+        label="Numero"
+        type="number"
+        variant="outlined"
+        value={form['numero']}
+        onChange={onChangeInput}
+        placeholder="Numero"
+        name={'numero'}
+      />
+
+      <InputsCadastroEndereco
+        required
+        id="outlined-required"
+        label="Complemento"
+        variant="outlined"
+        value={form['nomebloco']}
+        onChange={onChangeInput}
+        placeholder="Apto. / Bloco"
+        name={'nomebloco'}
+      />
+
+      <InputsCadastroEndereco
+        required
+        id="outlined-required"
+        label="Bairro"
+        variant="outlined"
+        value={form['bairro']}
+        onChange={onChangeInput}
+        placeholder="Bairro"
+        name={'bairro'}
+      />
+
+      <InputsCadastroEndereco
+        required
+        id="outlined-required"
+        label="Cidade"
+        variant="outlined"
+        value={form['cidade']}
+        onChange={onChangeInput}
+        placeholder="Cidade"
+        name={'cidade'}
+      />
+
+      <InputsCadastroEndereco
+        required
+        id="outlined-required"
+        label="Estado"
+        variant="outlined"
+        value={form['estado']}
+        onChange={onChangeInput}
+        placeholder="Estado"
+        name={'estado'}
+      />
+
+      <BotaoCadastroEndereco
+        variant="contained"
+        color="primary"
+        onClick={irParaFeed}
+      >
+        Salvar
+      </BotaoCadastroEndereco>
+      <LinkCadastro to="/cadastro"></LinkCadastro>
     </FormContainerCadastroEndereco>
-    
-        )  
-    }
-    
-export default TelaCadastroEnd
+  );
+};
+
+export default TelaCadastroEnd;
---
+++ src/components/8. Tela Editar Perfil/styles.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+import TextField from '@material-ui/core/TextField';
+import { Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+export const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LogoContainer = styled.img`
+  width: 7rem;
+  height: 4rem;
+  margin: 1rem;
+`;
+
+export const TituloEntrar = styled.h4`
+  margin: 1rem 1rem 2rem 1rem;
+`;
+
+export const InputsLogin = styled(TextField)`
+  width: 21rem;
+  height: 4rem;
+  margin: 0.5rem 1rem 0.5rem 1rem;
+`;
+
+export const BotaoSalvar = styled(Button)`
+  width: 21rem;
+  height: 3rem;
+  margin: 0.5rem 1rem 0.5rem 1rem;
+  background-color: #e8222e;
+  border: 0;
+  border-radius: 2px;
+  color: black !important;
+  text-transform: none !important;
+  font-weight: 600 !important;
+`;
